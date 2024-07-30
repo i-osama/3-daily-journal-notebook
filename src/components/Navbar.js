161669472bf5975158159c1d6a5124cf62f3608e
i-osama <zeeshan2k01@gmail.com>
2024-07-30 13:37:22 +0900

    Logout function added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,50 @@
 import React, { useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const location = useLocation();
+  let navigate = useNavigate()
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(location.pathname);
-  },[location]);
+  }, [location]);
+
+  const handleLogout = ()=>{
+    localStorage.removeItem('token');
+    window.location.reload(true);
+    navigate('/');
+  }
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">The Journals</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className={`nav-link ${location.pathname==="/"?"Active":""}`} aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className={`nav-link ${location.pathname==="/about"?"Active":""}`} to="/about">About</Link>
-        </li>
-        
-      
-      </ul>
-      <form className="d-flex" role="search">
-        {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">The Journals</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === "/" ? "Active" : ""}`} aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === "/about" ? "Active" : ""}`} to="/about">About</Link>
+            </li>
+
+
+          </ul>
+          {!localStorage.getItem('token')?<form className="d-flex" role="search">
+            {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button> */}
-        
 
 
-<Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
-<Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
-      </form>
-    </div>
-  </div>
-</nav>
+
+            <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+            <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
+          </form>:<button onClick={handleLogout} className='btn btn-primary'>Logout</button>}
+        </div>
+      </div>
+    </nav>
   )
 }
 
